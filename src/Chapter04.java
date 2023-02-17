7673accc15502a8b3b31e5a8107630a17cef34e0
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-02-18 00:02:00 +0900

    4章【変数を学ぼう】

---
+++ src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name1 = "tateishi";
+		System.out.println(name1);
+		
+		name1 = "Revolution";
+		System.out.println(name1);
+		
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
