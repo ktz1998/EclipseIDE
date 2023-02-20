cbfe8b3624b0e48ca0e815805dd0ce14a276a90f
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-02-20 23:30:32 +0900

    5章【型の基本を学ぼう】

---
+++ src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 123456789L;
+		double double1 = 321321.11d;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 10.5f;
+		Integer int1 = 6;
+		Character char1 = 'お';
+		System.out.println(int1);
+		System.out.println(float1);
+		System.out.println(char1);
+		
+		int[] array = new int[] {1,30,500,1200,5000};
+		int i = array[3];
+		System.out.println(i);
+		System.out.println(array[3]);
+	}
+
+}
