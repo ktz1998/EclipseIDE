e0971413f04a91463641764af8b11d2ca670ff0c
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-02-24 17:23:52 +0900

    条件分岐 if-elseまで

---
+++ src/If02.java
@@ -0,0 +1,34 @@
+
+public class If02 {
+	public static void main(String[] args) {
+		/* else-if文、else文の基本構文↓
+		 * if (条件式) {
+			    ブロック(処理文);
+			} else if (条件式) {
+			    ブロック(処理文);
+			} else if (条件式) {
+			    ブロック(処理文);
+			} else {
+			    ブロック(処理文)
+			}
+		 */
+		int number = 7;
+		if (number < 5) {
+			System.out.println(number + "は5未満です");
+		} else if (number < 10) {
+			System.out.println(number + "は5以上、10未満です");
+		} else if (number < 15) {
+			System.out.println(number + "は10以上、15未満です");
+		} else {
+			System.out.println(number + "は15未満ではないです");
+		}
+		
+		/*
+		 * if文の条件式を評価した結果が「false」の場合、
+		 * 最初のelse-if文の条件式を評価し、結果が「true」の場合は
+		 * ブロック内の処理が実行され、「false」の場合は次のelse-if文の条件式を評価.
+		 * 
+		 */
+	}
+
+}
