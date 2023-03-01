3c9270ab06fd352805f06b834701abec5183a638
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-03-01 23:29:37 +0900

    ｍｎ

--- src/For01.java
+++ src/For01.java
@@ -10,6 +10,10 @@ public class For01 {
 		for (int number = 1; number <= 5; number++) {
 			System.out.println("For01 = " + number);
 		}
+		
+		/* for文
+		 * while文と同じように指定の条件が「true」の間、繰り返し処理を行なう制御文.
+		 */
 	}
 
 }
