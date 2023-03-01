5070f9cfd936c79ebad99786772acd175cc5afd5
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-03-01 23:29:28 +0900

    8章for文

---
+++ src/For01.java
@@ -0,0 +1,15 @@
+
+public class For01 {
+	public static void main(String[] args) {
+		/* for文の基本構文↓
+		   for (初期化式; 条件式; 変化式) {
+				ブロック(繰り返し処理)
+			}
+		 */
+		
+		for (int number = 1; number <= 5; number++) {
+			System.out.println("For01 = " + number);
+		}
+	}
+
+}
