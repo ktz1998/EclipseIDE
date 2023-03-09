928a50cdeb77970da6104388d52cdbfc03ffcea9
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-03-09 21:25:19 +0900

    9章その他

---
+++ src/classMethod/human/Human04.java
@@ -0,0 +1,10 @@
+package classMethod.human;
+
+public class Human04 {
+	   //static変数
+	static public int humanCount = 0;
+	public String name;
+	   //定数
+	public static final String GREETING = "こんにちは";
+
+}
