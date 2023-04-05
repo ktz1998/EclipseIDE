153efd9c5a646f64b48486a59d9752cd8df47a12
ktz1998 <doraemonfan.kosuke@gmail.com>
2023-04-05 23:49:40 +0900

    13章.継承

--- src/extend/human/Employee.java
+++ src/extend/human/Employee.java
@@ -1,16 +1,21 @@
 package extend.human;
 
+//Humanクラスを継承したEmployeeクラス
 public class Employee extends Human {
-    private String department;
+ // サブクラスで追加したフィールド
+ private String department;
 
-    public Employee(String name, int age, String department) {
-        super(name, age);
-        this.department = department;
-    }
+ public Employee(String name, int age, String department) {
+     // スーパークラスのコンストラクタを呼び出し
+     super(name, age);
+     this.department = department;
+ }
 
-    public String getEmployeeProfile() {
-        String profile = "年齢は" + this.age + "です。";
-        profile += "サラリーマンで、部署は" + this.department + "です。";
-        return profile;
-    }
+ // サブクラスで追加したメソッド
+ public String getEmployeeProfile() {
+     // 親インスタンスのフィールドをthis変数で参照
+     String profile = "年齢は" + this.age + "です。";
+     profile += "サラリーマンで、部署は" + this.department + "です。";
+     return profile;
+ }
 }
